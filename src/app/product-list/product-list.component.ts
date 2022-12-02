4de35e1961dfab0aae43cd44d30d4c558397485b
faisal <nmr.njd22@gmail.com>
2022-12-02 22:11:41 +0300

    Added products list logic

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../modules/products';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  productList: Products[] = [];
+  productsQuantity: number[] = [];
+  constructor(private productsService: ProductService) {}
 
+  //call products
+  ngOnInit(): void {
+    this.productsQuantity = this.productsService.productsQuantity;
+    this.productsService
+      .getAllProducts()
+      .subscribe((data) => (this.productList = data));
+  }
 }
