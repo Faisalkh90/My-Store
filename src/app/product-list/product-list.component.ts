dc7fd939dbe7164a91c7986709f06d49583818ae
faisal <nmr.njd22@gmail.com>
2022-12-04 09:33:54 +0300

    update functionality for list of products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Products } from '../modules/products';
 import { ProductService } from '../services/product.service';
+import { CartService } from '../services/cart.service';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,14 +10,25 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductListComponent implements OnInit {
   productList: Products[] = [];
-  productsQuantity: number[] = [];
-  constructor(private productsService: ProductService) {}
+  constructor(
+    private productsService: ProductService,
+    private cartService: CartService
+  ) {}
 
-  //call products
+  //call products and set all to 1
   ngOnInit(): void {
-    this.productsQuantity = this.productsService.productsQuantity;
-    this.productsService
-      .getAllProducts()
-      .subscribe((data) => (this.productList = data));
+    this.productsService.getAllProducts().subscribe((data) => {
+      for (let product = 0; product < data.length; product++) {
+        const item = data[product];
+        item.quantity = 1;
+      }
+      this.productList = data;
+      this.productsService.setProducts(data);
+    });
+  }
+
+  // add products into user cart
+  addProduct(product: Products): void {
+    this.cartService.addProduct(product);
   }
 }
