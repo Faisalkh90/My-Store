5e2ed40f8fb36a0b932be7e86ae4815f807f717e
faisal <nmr.njd22@gmail.com>
2022-12-04 12:34:13 +0300

    sharing data between child and parent in product component

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Products } from '../modules/products';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  quantity: number;
+  productsQuantity: number[];
+  @Input() product: Products;
+  @Output() addProduct = new EventEmitter();
+  constructor(private productService: ProductService) {
+    this.quantity = 1;
+    this.productsQuantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.product = {
+      id: 1,
+      name: '',
+      quantity: 1,
+      url: '',
+      price: 1,
+      description: '',
+    };
+  }
+  ngOnInit(): void {}
 
+  // add product item into user cart
+  addedOnSubmit(product: Products): void {
+    product.quantity = this.quantity;
+    if (this.productsQuantity.length > 0) {
+      this.addProduct.emit(product);
+      window.alert(
+        ` ${this.product.quantity} for ${this.product.name} added successfully`
+      );
+      //reset the quantity to 1
+      this.quantity = 1;
+    } else {
+      console.log('check condition ');
+    }
+  }
 }
