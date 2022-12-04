9936f23b0c2e283003d3934f13bb2796d93101b8
faisal <nmr.njd22@gmail.com>
2022-12-04 13:16:29 +0300

    added order confirmation after user confirm their order

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Cart } from '../modules/cart';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css']
+  styleUrls: ['./confirmation.component.css'],
 })
-export class ConfirmationComponent {
-
+export class ConfirmationComponent implements OnInit {
+  order: Cart;
+  constructor(private cartService: CartService, private router: Router) {
+    this.order = {
+      id: 1,
+      price: 1,
+      creditCard: 1,
+      fullName: '',
+    };
+  }
+  ngOnInit(): void {
+    this.order = this.cartService.getOrder();
+  }
+  //clear user cart after successfully order
+  deleteCart(): void {
+    this.cartService.emptyCart();
+    this.router.navigate(['/']);
+  }
 }
