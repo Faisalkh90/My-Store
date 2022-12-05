85cd3c540e51242be05edaa3666799b4e8416df8
faisal <nmr.njd22@gmail.com>
2022-12-05 10:54:17 +0300

    update addProduct function with condition

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,18 +10,17 @@ export class CartService {
   cart!: Cart;
   constructor() {}
 
-  // add product to cart
+  // add product to cart and update user cart
   addProduct(product: Products): void {
     try {
-      const itemProduct = this.productList.find(
-        (data) => data.id == product.id
-      );
-      if (itemProduct) {
-        if (itemProduct.quantity && product.quantity) {
-          itemProduct.quantity += product.quantity;
-        } else {
-          this.productList.push(product);
-        }
+      const itemProduct = this.productList.filter((data) => {
+        return data.id == product.id;
+      })[0];
+
+      if (itemProduct?.quantity > 0) {
+        itemProduct.quantity = itemProduct.quantity + product.quantity;
+      } else {
+        this.productList.push(product);
       }
     } catch (err) {
       throw new Error(`Something went wrong in adding `);
